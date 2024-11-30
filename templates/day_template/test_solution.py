985cf46cb10b77804832fceccb28d96a6967f19a
Szymon Cader <szymon.sc.cader@gmail.com>
2024-11-30 19:29:49 +0100

    Clean up repo

--- templates/day_template/test_solution.py
+++
@@ -1,9 +0,0 @@
-from aoc.base_tests import BaseTestChallenge, Empty
-
-from . import Challenge
-
-
-class TestChallenge(BaseTestChallenge):
-    challenge_class = Challenge
-    expected_results_from_test_data = (Empty, Empty)
-    expected_results_from_real_data = (Empty, Empty)
